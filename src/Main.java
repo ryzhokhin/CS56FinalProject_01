7a2013998048441777162462730a0303ac967374
Woody <woodyaung2005@gmail.com>
2024-01-24 20:23:42 -0800

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1000000000");
+        System.out.println("100000099990");
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
 
