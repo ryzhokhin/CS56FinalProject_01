5d9a020865b46e0193e7e55fbb6982151f269555
Max <maxosteins@gmail.com>
2024-01-24 20:08:30 -0800

    Commit 04 Main -> test2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("10");
+        System.out.println("2");
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
-
-
         }
-        System.out.println("MAX Spoce");
+        System.out.println("MAX SPMacx");
     }
 }
